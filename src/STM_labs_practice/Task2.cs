2fc0fe8f605d1b2c4be2deab42018c12bfd682f3
Jopepka <mr_fisher2000@mail.ru>
2024-06-29 15:48:19 +0300

    done task 2

---
+++ src/STM_labs_practice/Task2.cs
@@ -0,0 +1,11 @@
+public static class Task2
+{
+    public static int[] GetCountAmebs()
+    {
+        int[] countAmebs = new int[24 / 3];
+        for (int i = 0; i < 24 / 3; i++)
+            countAmebs[i] = (int)Math.Pow(2, i + 1);
+
+        return countAmebs;
+    }
+}
