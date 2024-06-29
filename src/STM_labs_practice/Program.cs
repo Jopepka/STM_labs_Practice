54eb656d7485244aa9ce0c364850ac35dc60903c
Jopepka <mr_fisher2000@mail.ru>
2024-06-29 14:36:22 +0300

    solve

--- src/STM_labs_practice/Program.cs
+++ src/STM_labs_practice/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int countCoins = 0;
+        while (true)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Добро пожаловать в Грустного Хомячка");
+            Console.WriteLine("Вводите слово 'хом', чтобы получить 1$!");
+            Console.WriteLine("Чтобы вывести $ - закройте программу и они окажутся у вас под подушкой");
+            Console.WriteLine($"Баланс: {countCoins}$");
+
+            if (Console.ReadLine() == "хом")
+                countCoins++;
+        }
     }
 }
