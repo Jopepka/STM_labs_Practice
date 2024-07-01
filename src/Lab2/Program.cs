4c456b3937902886754c5211357ee609db350a5d
Alex <96999944+Jopepka@users.noreply.github.com>
2024-07-01 18:12:48 +0300

    done task1 (#3)
    * done task1
    
    * done task1
    
    * change a generic class to a generic method

--- src/Lab2/Program.cs
+++ src/Lab2/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("Задание 1. Сумма минимального и максимального.");
+        Console.WriteLine(Task1.SumMinAndMax(2, 10, 7));
+      
         Console.WriteLine("Задание 2. Количество амеб через 3, 6, ... 24 часа");
         Console.WriteLine(string.Join(", ", Task2.GetCountAmebs()));
     }
