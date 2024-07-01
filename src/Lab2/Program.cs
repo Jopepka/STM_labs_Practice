b3af31f1651b20a227b84b07cce040099e8b10cf
Jopepka <mr_fisher2000@mail.ru>
2024-07-01 18:08:18 +0300

    change a generic class to a generic method

--- src/Lab2/Program.cs
+++ src/Lab2/Program.cs
@@ -5,6 +5,6 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Задание 1. Сумма минимального и максимального.");
-        Console.WriteLine(Task1<double>.SumMinAndMax(2, 10, 7));
+        Console.WriteLine(Task1.SumMinAndMax(2, 10, 7));
     }
 }
--- src/Lab2/Task1.cs
+++ src/Lab2/Task1.cs
@@ -1,8 +1,8 @@
 using System.Numerics;
 
-public static class Task1<T> where T : INumber<T>
+public static class Task1
 {
-    public static T SumMinAndMax(T number1, T number2, T number3)
+    public static T SumMinAndMax<T>(T number1, T number2, T number3) where T : INumber<T>
     {
         T[] nums = [number1, number2, number3];
         return nums.Min() + nums.Max();
