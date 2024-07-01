7d7f1eb805dd2ee381831ec66261a8e40dc3ff21
Jopepka <mr_fisher2000@mail.ru>
2024-07-01 18:10:49 +0300

    change a generic class to a generic methods

--- src/Lab2/Task4.cs
+++ src/Lab2/Task4.cs
@@ -1,12 +1,12 @@
-public static class Task4<T>
+public static class Task4
 {
-    public static int GetCountOverlap(IEnumerable<T> plenty1, IEnumerable<T> plenty2)
+    public static int GetCountOverlap<T>(IEnumerable<T> plenty1, IEnumerable<T> plenty2)
     {
         CheckCorrectParams(plenty1, plenty2);
         return plenty1.Count(key => plenty2.Contains(key));
     }
 
-    private static void CheckCorrectParams(IEnumerable<T> plenty1, IEnumerable<T> plenty2)
+    private static void CheckCorrectParams<T>(IEnumerable<T> plenty1, IEnumerable<T> plenty2)
     {
         CheckNotNull(plenty1);
         CheckNotNull(plenty2);
@@ -21,7 +21,7 @@ public static class Task4<T>
             throw new ArgumentNullException();
     }
 
-    private static void CheckOnlyUniqluItems(IEnumerable<T> collection)
+    private static void CheckOnlyUniqluItems<T>(IEnumerable<T> collection)
     {
         if (collection.Count() != collection.Distinct().Count())
             throw new ArgumentException("The collection must contain unique values");
