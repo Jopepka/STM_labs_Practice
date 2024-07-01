010a434ce2da66b49f31a9c120e4299af05b9a33
Alex <96999944+Jopepka@users.noreply.github.com>
2024-07-01 18:02:38 +0300

    Lab1
    * solve
    
    * change project name
    
    * add .vs to .gitignore

---
+++ src/Lab1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace STM_labs_Practice;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int countCoins = 0;
+        while (true)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Добро пожаловать в Грустного Хомячка");
+            Console.WriteLine("Вводите слово 'хом', чтобы получить 1$!");
+            Console.WriteLine("Чтобы вывести $ - закройте программу и они окажутся у вас под подушкой");
+            Console.WriteLine($"Баланс: {countCoins}$");
+
+            if (Console.ReadLine() == "хом")
+                countCoins++;
+        }
+    }
+}
