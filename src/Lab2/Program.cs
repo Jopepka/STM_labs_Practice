2ee2ff2b0aad1c90c636fba869c65291ca5ce04a
Jopepka <mr_fisher2000@mail.ru>
2024-07-02 10:20:03 +0300

    move Task5.cs

--- src/Lab2/Program.cs
+++ src/Lab2/Program.cs
@@ -3,17 +3,23 @@
 class Program
 {
     static void Main(string[] args)
-    { 
+    {
         Console.WriteLine("Задание 1. Сумма минимального и максимального.");
         Console.WriteLine(Task1.SumMinAndMax(2, 10, 7));
-      
-        Console.WriteLine("Задание 2. Количество амеб через 3, 6, ... 24 часа");
+
+        Console.WriteLine("\nЗадание 2. Количество амеб через 3, 6, ... 24 часа");
         Console.WriteLine(string.Join(", ", Task2.GetCountAmebs()));
-      
-        Console.WriteLine("Задание 4. Количество общих элементов множества");
-        Console.WriteLine(Task4<int>.GetCountOverlap([1, 2, 3], [2, 3, 4]));
-        
-        Console.WriteLine("Задание 6. Двусвязный список");
+
+        Console.WriteLine("\nЗадание 3. Количество стрел");
+        Console.WriteLine($">->>--><--<<><: {Task3.CalculateArrows(">->>--><--<<><")}");
+
+        Console.WriteLine("\nЗадание 4. Количество общих элементов множества");
+        Console.WriteLine(Task4.GetCountOverlap([1, 2, 3], [2, 3, 4]));
+
+        Console.WriteLine("\nЗадание 5. Разница между двумя датами");
+        Console.WriteLine($"2024.07.02 -  now: {Task5.CountDays(new DateTime(2024, 07, 1), DateTime.Now)}");
+
+        Console.WriteLine("\nЗадание 6. Двусвязный список");
         MyList<int> myList = new MyList<int>([1, 2, 3, 4]);
         Console.WriteLine($"Изначальный список: {myList}");
 
