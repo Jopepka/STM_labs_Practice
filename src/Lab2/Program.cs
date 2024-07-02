c670590d3c1c78143ee3476567c11b38c949b22f
Alex <96999944+Jopepka@users.noreply.github.com>
2024-07-02 10:06:02 +0300

    done task 4 (#6)
    * done task 4
    
    * fix
    
    * remove unnecessary dependence
    
    * change a generic class to a generic methods

--- src/Lab2/Program.cs
+++ src/Lab2/Program.cs
@@ -9,5 +9,8 @@ class Program
       
         Console.WriteLine("Задание 2. Количество амеб через 3, 6, ... 24 часа");
         Console.WriteLine(string.Join(", ", Task2.GetCountAmebs()));
+      
+        Console.WriteLine("Задание 4. Количество общих элементов множества");
+        Console.WriteLine(Task4<int>.GetCountOverlap([1, 2, 3], [2, 3, 4]));
     }
 }
