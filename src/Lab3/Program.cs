4b98d17e6723307295e91cd5d0fe5ce3a685c8f5
Jopepka <mr_fisher2000@mail.ru>
2024-07-03 19:46:09 +0300

    done task 3

--- src/Lab3/Program.cs
+++ src/Lab3/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-
+        Console.WriteLine("Задание 3. Квадратичный трехчлен");
+        var quadraticEquation = Task3.GetQuadraticEquation(1, 2, 3); // 1*x^2 + 2*x + 3 
+        Console.WriteLine(quadraticEquation(2)); // 11
     }
 }
