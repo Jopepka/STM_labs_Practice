f3abe98ed541e71783e48043fdd64c5e15fb8f48
Alex <96999944+Jopepka@users.noreply.github.com>
2024-07-09 12:01:36 +0300

    done task 2

--- src/Lab3/Program.cs
+++ src/Lab3/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-
+        Console.WriteLine("Задание 2. Дни недели");
+        var dayOWeek = Task2.GetDayOWeekFunc();
+        Console.WriteLine(dayOWeek()); // Понедельник
+        Console.WriteLine(dayOWeek()); // Вторник
+        Console.WriteLine(dayOWeek()); // Среда
+        Console.WriteLine(dayOWeek()); // Четверг
+        Console.WriteLine(dayOWeek()); // Пятница
+        Console.WriteLine(dayOWeek()); // Суббота
+        Console.WriteLine(dayOWeek()); // Воскресень
+        Console.WriteLine(dayOWeek()); // Понедельник
+        Console.WriteLine(dayOWeek()); // Вторник
     }
 }
