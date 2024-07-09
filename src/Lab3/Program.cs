a5645a2dee831b0f38006a19e7b6a197da7daf44
Alex <96999944+Jopepka@users.noreply.github.com>
2024-07-09 12:04:11 +0300

    done task 4

--- src/Lab3/Program.cs
+++ src/Lab3/Program.cs
@@ -3,7 +3,7 @@
 class Program
 {
     static void Main(string[] args)
-    {      
+    {
         Console.WriteLine("Задание 2. Дни недели");
         var dayOWeek = Task2.GetDayOWeekFunc();
         Console.WriteLine(dayOWeek()); // Понедельник
@@ -19,5 +19,16 @@ class Program
         Console.WriteLine("Задание 3. Квадратичный трехчлен");
         var quadraticEquation = Task3.GetQuadraticEquation(1, 2, 3); // 1*x^2 + 2*x + 3 
         Console.WriteLine(quadraticEquation(2)); // 11
+        
+        Console.WriteLine("Задание 4. Слушатель и публикация событий");
+        var publisher1 = new Task4_Publisher("Publisher 1");
+        var publisher2 = new Task4_Publisher("Publisher 2");
+
+        var listener = new Task4_Listener();
+        publisher1.Subscribe(listener.ListenObject);
+        publisher2.Subscribe(listener.ListenObject);
+
+        publisher1.RaiseEvent();
+        publisher2.RaiseEvent();
     }
 }
