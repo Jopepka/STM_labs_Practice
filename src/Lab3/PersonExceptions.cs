eb80386960ce15897cce9f1ea1d7bb66df2cae12
Jopepka <mr_fisher2000@mail.ru>
2024-07-10 17:04:19 +0300

    realize exceptions

---
+++ src/Lab3/PersonExceptions.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+
+public class DataNotLoaded : InvalidOperationException { }
+
+public class InconsistentLimits : ArgumentException
+{
+    public readonly IEnumerable intersectionChars;
+    public InconsistentLimits(IEnumerable intersectionChars, string message) : base(message)
+    {
+        this.intersectionChars = intersectionChars;
+    }
+}
+
+public class IncorrectString : Exception
+{
+    public readonly int lineNumber;
+
+    public IncorrectString(int lineNumber, string message) : base(message)
+    {
+        this.lineNumber = lineNumber;
+    }
+}
+
+public class WrongStartingSymbol : IncorrectString
+{
+    public readonly char startCharOfLine;
+
+    public WrongStartingSymbol(char startCharOfLine, int lineNumber, string message) : base(lineNumber, message)
+    {
+        this.startCharOfLine = startCharOfLine;
+    }
+}
+
+public class TooManyProhibitedLines : Exception { }
