ead8ec1abfedd2f14542585df5ecd3242768ce34
Jopepka <mr_fisher2000@mail.ru>
2024-07-29 00:07:49 +0300

    done PLINQ

---
+++ src/Lab6/PlinqExample.cs
@@ -0,0 +1,33 @@
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+public class PlinqExample
+{
+    public int numPoints { get; set; } = 10000000;
+    public Random random { get; set; } = new Random(41);
+
+    readonly Stopwatch _stopwatch = new Stopwatch();
+
+    public void Start()
+    {
+        var points = Enumerable.Range(0, numPoints).Select(i => new { x = random.NextDouble(), y = random.NextDouble() }).ToArray();
+
+        _stopwatch.Restart();
+        int countSequential = points.Count(item => IsPointInCircle(item.x, item.y));
+        _stopwatch.Stop();
+
+        Console.WriteLine($"Последовательное вычисление числа Пи: {CalculatePi(countSequential)}");
+        Console.WriteLine($"Время последовательного выполнения: {_stopwatch.ElapsedMilliseconds} мс");
+
+        _stopwatch.Restart();
+        int countParallel = points.AsParallel().Count(item => IsPointInCircle(item.x, item.y));
+        _stopwatch.Stop();
+
+        Console.WriteLine($"Параллельное вычисление числа Пи: {CalculatePi(countParallel)}");
+        Console.WriteLine($"Время параллельного выполнения: {_stopwatch.ElapsedMilliseconds} мс");
+    }
+
+    private bool IsPointInCircle(double x, double y) => x * x + y * y <= 1;
+
+    private double CalculatePi(int countPointsInCircle) => 4.0 * countPointsInCircle / numPoints;
+}
--- src/Lab6/Program.cs
+++ src/Lab6/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace Lab6;
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-
+        Console.WriteLine("Task PLINQ");
+        // У меня выдает при последовательном 420мс, при параллельном 150мс
+        new PlinqExample().Start();
     }
 }
