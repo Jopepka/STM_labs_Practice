fb990ea6867dca8e51a83fa4f95e9f69c0a7c47a
Alex <96999944+Jopepka@users.noreply.github.com>
2024-07-30 17:31:09 +0300

    Lab 6. Tasks 1 to 8 (#19)
    * done
    
    * add example
    
    * change entitis

--- src/Lab6/Entity/City.cs
+++ src/Lab6/Entity/City.cs
@@ -3,4 +3,6 @@ public class City
     public int ID { get; set; }
     public string? Name { get; set; }
     public int CityCode { get; set; }
+
+    public override string ToString() => $"{ID}. {Name}, cityPhone: {CityCode}";
 }
