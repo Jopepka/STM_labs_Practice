e18d873c22e2defa55be0b1b754c2c485d1779b8
Jopepka <mr_fisher2000@mail.ru>
2024-07-31 17:25:27 +0300

    refactoring

---
+++ src/Lab7/Lab7_WPF/ConsoleProccessHeandler.cs
@@ -0,0 +1,50 @@
+﻿
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Windows.Documents;
+
+namespace Lab7_WPF
+{
+    internal class ConsoleProccessHeandler
+    {
+        private Process? _consoleProcess;
+        public bool IsRun { get; private set; }
+
+        public void StartConsoleApp(string consolePath, int tactReadDelay, int countReadCharsInTact, DataReceivedEventHandler handler)
+        {
+            if (IsRun)
+                return;
+
+            var processInfo = new ProcessStartInfo()
+            {
+                FileName = "E:\\Programming\\STM_labs_Practice\\src\\Lab7\\Lab7_Console\\bin\\Debug\\net8.0\\Lab7_Console.exe",
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                CreateNoWindow = true,
+                Arguments = $"{consolePath} {tactReadDelay} {countReadCharsInTact}",
+            };
+
+            _consoleProcess = new Process();
+            _consoleProcess.StartInfo = processInfo;
+            _consoleProcess.OutputDataReceived += handler;
+            _consoleProcess.Start();
+            _consoleProcess.BeginErrorReadLine();
+            _consoleProcess.BeginOutputReadLine();
+
+            IsRun = true;
+        }
+
+        public void StartThread() => SendCommandToConsole($"START_THREAD");
+        public void StopLastThread() => SendCommandToConsole($"STOP_THREAD");
+
+        public void StopConsoleApp() => _consoleProcess.Kill();
+
+        private void SendCommandToConsole(string command)
+        {
+            if (_consoleProcess != null && !_consoleProcess.HasExited)
+                _consoleProcess.StandardInput.WriteLine(command);
+        }
+    }
+}
--- src/Lab7/Lab7_WPF/MainWindow.xaml.cs
+++ src/Lab7/Lab7_WPF/MainWindow.xaml.cs
@@ -6,16 +6,14 @@ using System.Windows.Threading;
 
 namespace Lab7_WPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        private Process consoleProcess;
         private int threadCount = 0;
         private List<string> allOutput = new List<string>();
+        private ConcurrentQueue<string> outputQueue = new ConcurrentQueue<string>();
 
-        private DispatcherTimer uiUpdateTimer;
+        private ConsoleProccessHeandler consoleProcess = new ConsoleProccessHeandler();
+        private DispatcherTimer uiUpdateTimer = new DispatcherTimer();
 
         public MainWindow()
         {
@@ -29,28 +27,47 @@ namespace Lab7_WPF
             DelayTextBox.Text = "1000";
             ThreadCountTextBox.Text = "2";
 
-            uiUpdateTimer = new DispatcherTimer();
             uiUpdateTimer.Interval = TimeSpan.FromMilliseconds(500);
             uiUpdateTimer.Tick += UiUpdateTimer_Tick;
             uiUpdateTimer.Start();
         }
 
-        private void UiUpdateTimer_Tick(object sender, EventArgs e)
-        {
-            while (outputQueue.TryDequeue(out string data))
-            {
-                allOutput.Add(data);
-            }
+        private string GetConsolePath() => "E:\\Programming\\STM_labs_Practice\\src\\Lab7\\Lab7_WPF\\WarAndPeace.txt";
+        private int GetTactReadDelay() => int.Parse(DelayTextBox.Text);
+        private int GetCountReadCharsInTact() => int.Parse(ItemsPerIterationTextBox.Text);
+        private int GetCountStartThreads() => int.Parse(ThreadCountTextBox.Text);
+        private string GetSelectedNameThread() => ThreadComboBox.Text;
+        private void UpdateDisplayOutput(string text) => OutputTextBox.Text = text;
 
-            FilterAndDisplayOutput(GetSelectedNameThread(), GetSelectedNameThread() == "All threads");
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!consoleProcess.IsRun)
+                consoleProcess.StartConsoleApp(GetConsolePath(), GetTactReadDelay(), GetCountReadCharsInTact(), ConsoleProcess_OutputDataReceived);
+
+            for (int i = 0; i < GetCountStartThreads(); i++)
+            {
+                consoleProcess.StartThread();
+                AddThreadInThreadComboBox();
+            }
         }
 
-        private void ThreadComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) => FilterAndDisplayOutput(GetSelectedNameThread(), GetSelectedNameThread() == "All threads");
+        // ThreadComboBox.Items.Count - 1 т.к. в комбо-боксе уже лежат All threads и Main Thread, а счет ведется с 1
+        private void AddThreadInThreadComboBox() =>
+            ThreadComboBox.Items.Add($"Thread {ThreadComboBox.Items.Count - 1}");
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            consoleProcess.StopLastThread();
+            ThreadComboBox.Items.RemoveAt(ThreadComboBox.Items.Count - 1);
+        }
+
+        private void ThreadComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) =>
+            FilterAndDisplayOutput(GetSelectedNameThread(), GetSelectedNameThread() == "All threads");
 
         private void FilterAndDisplayOutput(string filterBy, bool showAll = false)
         {
             var filterText = string.Join("", showAll ? allOutput : DeletePrefixInCommandResultRow(Filter(filterBy)));
-            DisplayOutput(filterText);
+            UpdateDisplayOutput(filterText);
         }
 
         private IEnumerable<string> Filter(string? containString) =>
@@ -58,95 +75,22 @@ namespace Lab7_WPF
 
         private IEnumerable<string> DeletePrefixInCommandResultRow(IEnumerable<string> rows) => rows.Select(row => row.Split(": ")[1]);
 
-        private void DisplayOutput(string text) => OutputTextBox.Text = text;
-
-        private string? GetSelectedNameThread() => ThreadComboBox.Text;
-
-        private void StartButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (IsExistConsoleProcess())
-                StartConsoleApp(GetConsolePath(), GetTactReadDelay(), GetCountReadCharsInTact());
-
-            for (int i = 0; i < GetCountStartThreads(); i++)
-                StartThread();
-        }
-
-        private bool IsExistConsoleProcess() => consoleProcess == null || consoleProcess.HasExited;
-
-        private string GetConsolePath() => "E:\\Programming\\STM_labs_Practice\\src\\Lab7\\Lab7_WPF\\WarAndPeace.txt";
-
-        private int GetTactReadDelay() => int.Parse(DelayTextBox.Text);
-
-        private int GetCountReadCharsInTact() => int.Parse(ItemsPerIterationTextBox.Text);
-
-        private int GetCountStartThreads() => int.Parse(ThreadCountTextBox.Text);
-
-        private void StartConsoleApp(string consolePath, int tactReadDelay, int countReadCharsInTact)
-        {
-            var processInfo = new ProcessStartInfo()
-            {
-                FileName = "E:\\Programming\\STM_labs_Practice\\src\\Lab7\\Lab7_Console\\bin\\Debug\\net8.0\\Lab7_Console.exe",
-                RedirectStandardInput = true,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                CreateNoWindow = true,
-                Arguments = $"{consolePath} {tactReadDelay} {countReadCharsInTact}",
-            };
-
-            consoleProcess = new Process();
-            consoleProcess.StartInfo = processInfo;
-            consoleProcess.OutputDataReceived += ConsoleProcess_OutputDataReceived;
-            consoleProcess.Start();
-            consoleProcess.BeginErrorReadLine();
-            consoleProcess.BeginOutputReadLine();
-        }
-
-        private void StartThread()
-        {
-            threadCount++;
-            ThreadComboBox.Items.Add($"Thread {threadCount}");
-            SendCommandToConsole($"START_THREAD");
-        }
-
-        private void StopButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (threadCount > 0)
-            {
-                SendCommandToConsole("STOP_THREAD");
-                threadCount--;
-                ThreadComboBox.Items.RemoveAt(ThreadComboBox.Items.Count - 1);
-            }
-            else if (consoleProcess != null && !consoleProcess.HasExited)
-            {
-                consoleProcess.Kill();
-                consoleProcess = null;
-            }
-        }
-
-        private void SendCommandToConsole(string command)
-        {
-            if (consoleProcess != null && !consoleProcess.HasExited)
-                consoleProcess.StandardInput.WriteLine(command);
-        }
-
-
-        private ConcurrentQueue<string> outputQueue = new ConcurrentQueue<string>();
-
         private void ConsoleProcess_OutputDataReceived(object sender, DataReceivedEventArgs e)
         {
             if (!string.IsNullOrEmpty(e.Data))
-            {
-                Dispatcher.BeginInvoke(() =>
-                {
-                    allOutput.Add(e.Data);
-                    FilterAndDisplayOutput(GetSelectedNameThread(), GetSelectedNameThread() == "All threads");
-                });
-            }
+                outputQueue.Enqueue(e.Data);
+        }
+        private void UiUpdateTimer_Tick(object sender, EventArgs e)
+        {
+            while (outputQueue.TryDequeue(out string data))
+                allOutput.Add(data);
+
+            FilterAndDisplayOutput(GetSelectedNameThread(), GetSelectedNameThread() == "All threads");
         }
 
         private void Window_Closed(object sender, EventArgs e)
         {
-            consoleProcess.Kill();
+            consoleProcess.StopConsoleApp();
         }
     }
 }
