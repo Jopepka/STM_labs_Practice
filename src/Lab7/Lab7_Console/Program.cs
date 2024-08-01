532f89d29bfbf41ae446300f93b8292ececf3818
Jopepka <mr_fisher2000@mail.ru>
2024-08-01 17:08:18 +0300

    refactoring

--- src/Lab7/Lab7_Console/Program.cs
+++ src/Lab7/Lab7_Console/Program.cs
@@ -1,77 +1,47 @@
-﻿class Program
+﻿using Lab7_Console;
+using Lab7_Console.Task;
+using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+class Program
 {
     static List<Thread> threads = new List<Thread>();
     static bool running = true;
 
     static string filePath;
-    static int millisecondDelay;
-    static int itemsPerIteration;
+    static int millisecondTactDelay;
+    static int itemsPerTact;
     static int countThread = 0;
 
     static void Main(string[] args)
     {
-        Console.WriteLine($"Thread Main: Count args = {args.Length}");
-        Console.WriteLine("Thread Main: args:");
-        foreach (var item in args)
-            Console.WriteLine($"Thread Main: {item}");
+        Console.OutputEncoding = Encoding.Default;
 
         filePath = args[0];
-        millisecondDelay = int.Parse(args[1]);
-        itemsPerIteration = int.Parse(args[2]);
+        millisecondTactDelay = int.Parse(args[1]);
+        itemsPerTact = int.Parse(args[2]);
 
-        Run();
+        var threadManager = new ThreadManager(ReadFileTask);
+        threadManager.Subscribe(Console.WriteLine);
+        threadManager.Run(GetCommandsFromConsole);
     }
 
-    private static void Run()
-    {
-        Console.WriteLine("Thread Main: The application is running");
-        while (running)
-        {
-            Console.WriteLine("Thread Main: Wait a command");
+    static IThreadControlled ReadFileTask() => new ReadFile(filePath, itemsPerTact, millisecondTactDelay, Console.WriteLine);
 
+    static Commands GetCommandsFromConsole()
+    {
+        while (true)
+        {
             switch (Console.ReadLine())
             {
-                case "START_THREAD":
-                    StartThread();
-                    break;
-                case "STOP_THREAD":
-                    StopThread();
-                    break;
-                case "EXIT":
-                    running = false;
-                    break;
+                case "START_NEW_THREAD":
+                    return Commands.StartNewThread;
+                case "STOP_LAST_THREAD":
+                    return Commands.StopLastThread;
                 default:
-                    Console.WriteLine("Thread Main: Wrong command");
-                    break;
+                    continue;
             }
         }
-    }
-
-    private static void StartThread()
-    {
-        Thread thread = new Thread(() => ReadFile(filePath, itemsPerIteration, millisecondDelay));
-        threads.Add(thread);
-        thread.Name = threads.Count().ToString();
-        thread.Start();
-    }
-
-    public static void ReadFile(string filePath, int itemsPerIteration, int millisecondDelay)
-    {
-        using StreamReader sr = new StreamReader(filePath);
-        while (sr.Peek() != -1)
-        {
-            var resStr = "";
-            for (int i = 0; i < itemsPerIteration && sr.Peek() != -1; i++)
-                resStr += (char)sr.Read();
-
-            Console.WriteLine($"Thread {Thread.CurrentThread.Name}: {resStr}");
-            Thread.Sleep(millisecondDelay);
-        }
-    }
-
-    private static void StopThread()
-    {
-        threads[^1].Abort();
-        threads.RemoveAt(threads.Count - 1);
     }
 }
---
+++ src/Lab7/Lab7_Console/ThreadTask.cs
@@ -0,0 +1,23 @@
+﻿using Lab7_Console.Task;
+
+namespace Lab7_Console
+{
+    internal class ThreadTask
+    {
+        IThreadControlled _doTask;
+        Thread? _thread;
+
+        public ThreadTask(IThreadControlled doTask) => _doTask = doTask;
+
+        public void ThreadStart()
+        {
+            if (_thread == null)
+            {
+                _thread = new Thread(_doTask.Start);
+                _thread.Start();
+            }
+        }
+
+        public void ThreadStop() => _doTask.Stop();
+    }
+}
