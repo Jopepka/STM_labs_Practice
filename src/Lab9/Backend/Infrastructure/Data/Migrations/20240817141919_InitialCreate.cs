721d3977738968c52653a0981530adbbfb5b5481
Jopepka <mr_fisher2000@mail.ru>
2024-08-17 19:32:40 +0300

    add migrations

---
+++ src/Lab9/Backend/Infrastructure/Data/Migrations/20240817141919_InitialCreate.cs
@@ -0,0 +1,179 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Buildings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Address = table.Column<string>(type: "text", nullable: false),
+                    Latitude = table.Column<double>(type: "double precision", nullable: false),
+                    Longitude = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Buildings", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CategoryName = table.Column<string>(type: "text", nullable: false),
+                    ParentCategoryId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CompanyPhones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Phone = table.Column<string>(type: "text", nullable: false),
+                    CompanyId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompanyPhones", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CompanyName = table.Column<string>(type: "text", nullable: false),
+                    BuildingId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Companies_Buildings_BuildingId",
+                        column: x => x.BuildingId,
+                        principalTable: "Buildings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CompanyCategories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CompanyId = table.Column<int>(type: "integer", nullable: false),
+                    CategoryId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompanyCategories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CompanyCategories_Companies_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Companies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Buildings",
+                columns: new[] { "Id", "Address", "Latitude", "Longitude" },
+                values: new object[,]
+                {
+                    { 1, "Bluchera 32/1", 55.751244, 37.618423 },
+                    { 2, "Tverskaya 7", 55.765219999999999, 37.605559999999997 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "CategoryName", "ParentCategoryId" },
+                values: new object[,]
+                {
+                    { 1, "Food", null },
+                    { 2, "Semi-finished products wholesale", 1 },
+                    { 3, "Meat products", 1 },
+                    { 4, "Automobiles", null }
+                });
+
+            migrationBuilder.InsertData(
+                table: "CompanyPhones",
+                columns: new[] { "Id", "CompanyId", "Phone" },
+                values: new object[,]
+                {
+                    { 1, 1, "2-222-222" },
+                    { 2, 1, "3-333-333" },
+                    { 3, 1, "8-923-666-13-13" },
+                    { 4, 2, "4-444-444" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "BuildingId", "CompanyName" },
+                values: new object[,]
+                {
+                    { 1, 1, "ООО Рога и Копыта" },
+                    { 2, 2, "ООО Машины и Механизмы" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "CompanyCategories",
+                columns: new[] { "Id", "CategoryId", "CompanyId" },
+                values: new object[,]
+                {
+                    { 1, 2, 1 },
+                    { 2, 3, 1 },
+                    { 3, 4, 2 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_BuildingId",
+                table: "Companies",
+                column: "BuildingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompanyCategories_CompanyId",
+                table: "CompanyCategories",
+                column: "CompanyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "CompanyCategories");
+
+            migrationBuilder.DropTable(
+                name: "CompanyPhones");
+
+            migrationBuilder.DropTable(
+                name: "Companies");
+
+            migrationBuilder.DropTable(
+                name: "Buildings");
+        }
+    }
+}
